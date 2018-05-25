4dbae7f4cf1a68c11ba29ea450df701e65afdc65
lidog <15625503948@163.com>
2018-05-25 16:08:14 +0800

    update

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -17,9 +17,9 @@ const app = express()
 var appData = require('../src/data.json')//加载本地数据文件
 var userInfo = appData.userInfo;
 var router = appData.router;
+var table = appData.table;
 var apiRoutes = express.Router()
 app.use('/api', apiRoutes)
-
 
 
 const HOST = process.env.HOST
@@ -27,7 +27,7 @@ const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+    rules: utils.styleLoaders({sourceMap: config.dev.cssSourceMap, usePostCSS: true})
   },
   // cheap-module-eval-source-map is faster for development
   devtool: config.dev.devtool,
@@ -37,7 +37,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
-        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
+        {from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html')},
       ],
     },
     hot: true,
@@ -47,7 +47,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
     overlay: config.dev.errorOverlay
-      ? { warnings: false, errors: true }
+      ? {warnings: false, errors: true}
       : false,
     publicPath: config.dev.assetsPublicPath,
     proxy: config.dev.proxyTable,
@@ -59,24 +59,30 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       app.post('/api/user/login', function (req, res) { // 注意这里改为post就可以了
         res.json({
           errno: 0,
-          data:{
-              token:"firstUser",
-              userId:1
+          data: {
+            token: "firstUser",
+            userId: 1
           }
         });
       }),
-      app.get('/api/user', function (req, res) { // 注意这里改为post就可以了
-        res.json({
-          errno: 0,
-          data:userInfo
-        });
-      }),
-      app.get('/api/router', function (req, res) { // 注意这里改为post就可以了
-        res.json({
-          errno: 0,
-          data:router
-        });
-      })
+        app.get('/api/user', function (req, res) { // 注意这里改为post就可以了
+          res.json({
+            errno: 0,
+            data: userInfo
+          });
+        }),
+        app.get('/api/router', function (req, res) { // 注意这里改为post就可以了
+          res.json({
+            errno: 0,
+            data: router
+          });
+        }),
+        app.get('/api/table', function (req, res) { // 注意这里改为post就可以了
+          res.json({
+            errno: 0,
+            data: table
+          });
+        })
     }
   },
   plugins: [
@@ -120,8 +126,8 @@ module.exports = new Promise((resolve, reject) => {
           messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
         },
         onErrors: config.dev.notifyOnErrors
-        ? utils.createNotifierCallback()
-        : undefined
+          ? utils.createNotifierCallback()
+          : undefined
       }))
 
       resolve(devWebpackConfig)
